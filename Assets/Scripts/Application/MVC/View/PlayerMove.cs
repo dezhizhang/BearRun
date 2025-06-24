86dc2dddabc22d7d43ec42857b369e5a718724d4
dezhizhang <1541609448@qq.com>
2025-06-25 05:22:23 +0800

    feat:添加按键输入GetInputDirection

--- Assets/Scripts/Application/MVC/View/PlayerMove.cs
+++ Assets/Scripts/Application/MVC/View/PlayerMove.cs
@@ -1,23 +1,24 @@
+using System;
 using UnityEngine;
 using static Consts;
+
 public class PlayerMove : View
 {
     // 人物移动速度
     public float speed = 20.0f;
     private CharacterController _mcc;
-    
+
     private InputDirection _inputDir = InputDirection.Null;
-    
+
     private bool _activeInput = false;
 
     // 按下的位置
     private Vector3 _mousePos;
+
     // 获取视图名称
-    public override string Name  {
-        get
-        {
-            return V_PlayerMove;
-        }
+    public override string Name
+    {
+        get { return V_PlayerMove; }
     }
 
     public override void HandleEvent(string eventName, object data)
@@ -46,16 +47,50 @@ public class PlayerMove : View
             _mousePos = Input.mousePosition;
         }
 
+        // 手机输入法
         if (Input.GetMouseButton(0) && _activeInput)
         {
             Vector3 dir = Input.mousePosition - _mousePos;
             if (dir.magnitude > 20)
             {
-                
-                Debug.Log(dir);
+                if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y) && dir.x > 0)
+                {
+                    _inputDir = InputDirection.Right;
+                }
+                else if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y) && dir.x < 0)
+                {
+                    _inputDir = InputDirection.Left;
+                }
+                else if (Mathf.Abs(dir.x) < Mathf.Abs(dir.y) && dir.y > 0)
+                {
+                    _inputDir = InputDirection.Up;
+                }
+                else if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x) && dir.x < 0)
+                {
+                    _inputDir = InputDirection.Down;
+                }
+
+                _activeInput = false;
             }
         }
 
+        // 电脑按键输入
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.Space))
+        {
+            _inputDir = InputDirection.Up;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            _inputDir = InputDirection.Down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _inputDir = InputDirection.Left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _inputDir = InputDirection.Right;
+        }
+        _activeInput = false;
     }
-
 }
--- Assets/Scripts/Application/MVC/View/RoadChange.cs
+++ Assets/Scripts/Application/MVC/View/RoadChange.cs
@@ -9,23 +9,36 @@ public class RoadChange : MonoBehaviour
     [HideInInspector]
     public GameObject parent;
 
+
+   // TODO 
     private void Start()
     {
-        if (parent == null)
+        if (parent == null)  // 确保 parent 未初始化时才创建
         {
             parent = new GameObject();
             parent.transform.position = Vector3.zero;
             parent.name = "Road";
         }
         
-        // 初始化物体生成
-        roadNow = Game.Instance.objectPool.Spawn("Pattern_1",parent.transform);
-        roadNext = Game.Instance.objectPool.Spawn("Pattern_2",parent.transform);
-        
-        roadNext.transform.position += new Vector3(0, 0, 160);
-        
-        
+        if (Game.Instance != null && Game.Instance.objectPool != null)
+        {
+            Debug.Log("Game.Instance and objectPool are valid.");
+            roadNow = Game.Instance.objectPool.Spawn("Pattern_1", parent.transform);
+            roadNext = Game.Instance.objectPool.Spawn("Pattern_2", parent.transform);
+            roadNext.transform.position += new Vector3(0, 0, 160);
+        }
+        else
+        {
+            Debug.LogError("Game.Instance or objectPool is null.");
+        }
+    
+        // // 初始化物体生成
+        // roadNow = Game.Instance.objectPool.Spawn("Pattern_1", parent.transform);
+        // roadNext = Game.Instance.objectPool.Spawn("Pattern_2", parent.transform);
+        //
+        // roadNext.transform.position += new Vector3(0, 0, 160);
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
