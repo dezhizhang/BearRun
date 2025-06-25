4ad9d150409f57f83ffd47502962480667f1d776
dezhizhang <1541609448@qq.com>
2025-06-26 06:59:38 +0800

    feat:添加游戏滑动

--- Assets/Scripts/Application/MVC/View/PlayerMove.cs
+++ Assets/Scripts/Application/MVC/View/PlayerMove.cs
@@ -9,18 +9,21 @@ public class PlayerMove : View
 
     // 车道间的间距
     public float laneDistance = 2.0f;
+
     // 重力
     public float gravityValue = -9.81f;
+
     // 跳越的高度
     public float jumpHeight = 1.0f;
-    
+
     // 检测的层级
     public LayerMask groundLayer;
-    
+
     public Transform groundCheck;
+
     //检测的半径
     public float groundCheckRadius = 0.2f;
-    
+
 
     // 玩家当前所在的车道
     public int playerCurrentLane = 0;
@@ -29,15 +32,26 @@ public class PlayerMove : View
     private InputDirection _inputDir = InputDirection.Null;
 
     private bool _activeInput = false;
-    
+
     // 玩家是否在地面上
     private bool _isGround = true;
+
     // 重力系数数值
     private Vector3 _velocity;
 
+    public float speedAddDis = 100.0f;
+
+    public float speedAdd = 10.0f;
+
+    public float maxSpeedAdd = 60.0f;
 
     // 按下的位置
     private Vector3 _mousePos;
+
+    // 是否在滑动
+    public bool isSlide;
+    private float _distance = 0.0f;
+
 
     // 获取视图名称
     public override string Name
@@ -131,30 +145,37 @@ public class PlayerMove : View
                 break;
             case InputDirection.Up:
                 _velocity.y = _velocity.y + Mathf.Sqrt(jumpHeight * -3 * gravityValue);
-                SendMessage("AnimManager",_inputDir);
+                SendMessage("AnimManager", _inputDir);
                 break;
             case InputDirection.Down:
-                SendMessage("AnimManager",_inputDir);
+                if (!isSlide)
+                {
+                    isSlide = true;
+                    Invoke("SlideEnd", 0.73f);
+                    SendMessage("AnimManager", _inputDir);
+                }
+
                 break;
             case InputDirection.Left:
                 MoveLane(false);
-                SendMessage("AnimManager",_inputDir);
+                SendMessage("AnimManager", _inputDir);
                 break;
             case InputDirection.Right:
+                // 如果在滑动
                 MoveLane(true);
-                SendMessage("AnimManager",_inputDir);
+                SendMessage("AnimManager", _inputDir);
                 break;
             default:
                 break;
         }
-
-      
     }
 
 
     // 玩家移动
     private void PlayerDoMove()
     {
+        UpdateSpeed();
+
         _isGround = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
 
         if (_isGround && _velocity.y < 0)
@@ -169,9 +190,9 @@ public class PlayerMove : View
         Vector3 moveDir = targetPos - transform.position;
 
         _mcc.Move((moveDir + transform.forward) * speed * Time.deltaTime);
-        
+
         _velocity.y = _velocity.y + gravityValue * Time.deltaTime;
-        _mcc.Move(_velocity *  Time.deltaTime);
+        _mcc.Move(_velocity * Time.deltaTime);
     }
 
     // 调整玩家移动车道
@@ -180,5 +201,24 @@ public class PlayerMove : View
         playerCurrentLane = playerCurrentLane + (right ? 1 : -1);
         // 约束移动的距离
         playerCurrentLane = Mathf.Clamp(playerCurrentLane, -1, 1);
+    }
+
+    // 滑动结束
+    private void SlideEnd()
+    {
+        isSlide = false;
+    }
+
+    private void UpdateSpeed()
+    {
+        if (speed < maxSpeedAdd)
+        {
+            _distance = _distance + speed * Time.deltaTime;
+            if (_distance > speedAddDis)
+            {
+                _distance = 0;
+                speed = speed + speedAdd;
+            }
+        }
     }
 }
